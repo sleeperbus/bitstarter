800fe6fede6173c04213d789834e7a9314739710
sleeperbus <sleeperbus@gmail.com>
2013-07-07 06:34:42 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(256);
+buffer = fs.readFileSync("index.html")
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString("utf-8"))
 });
 
 var port = process.env.PORT || 5000;
